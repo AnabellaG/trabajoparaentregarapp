9d9d55c0373ae12ac7a1474cf37e89881a03f5b7
AnabellaG <anabellasoledadgomez@gmail.com>
2019-06-06 23:17:14 -0300

    el boton de ir al sitio web funciona con un intent

--- app/src/main/java/com/example/myapplication/home4.java
+++ app/src/main/java/com/example/myapplication/home4.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.URI;
 
 public class home4 extends AppCompatActivity {
+
+    //declaro objetos
+    public Button boton_iralsitioweb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home4);
+
+        //referencio objetos
+        boton_iralsitioweb = findViewById(R.id.boton_iralsitioweb);
+
+        boton_iralsitioweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.adidas.com.ar") );
+                startActivity(intent);
+            }
+        });
+
     }
 }
+
+
+
+
